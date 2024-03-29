70336b1a805670ba3b050407a6773186dc5de5e1
wael-ajabi <ajabiwael1@gmail.com>
2024-03-29 23:56:07 +0000

    third

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <div>
          <div className="cover">
           <Button>
-          <h1>Click me</h1>
+          <h1>Click me on second branch</h1>
           </Button>
        <form className="formulaire" id="formul">
         <input onChange={handleChange} type="text" placeholder="Your Name" name="firstName" id="firstName"/>
